b946c530c1556692739ffbd04113500fb92ea9ba
Frederick Tan <tan@carnegiescience.edu>
2015-08-05 13:55:47 -0400

    Update doReset.sh

--- build/doReset.sh
+++ build/doReset.sh
@@ -11,7 +11,7 @@ cd /home/ubuntu/csaw
 rm csaw.bam.results.csv
 
 cd /home/ubuntu/genome
-rm -r chr20.*
+rm -r chr20*.*
 rm Log.out
 
 cd /home/ubuntu/hisat
@@ -21,4 +21,4 @@ cd /home/ubuntu/rawdata
 rm SRR1153470_?.fastq
 
 cd /home/ubuntu/star
-rm star.*
+rm star*.*
